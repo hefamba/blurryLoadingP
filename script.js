8e27f4d75b0992c147c4930225403d7ce990295d
hefamba <81347556+hefamba@users.noreply.github.com>
2023-08-17 17:08:38 -0500

    completed blurring

--- script.js
+++ script.js
@@ -10,7 +10,15 @@ const blurring = () => {
     if (loading > 99) {
         clearInterval(int);
     }
+
+    load.innerText = `${loading}%`;
+    load.style.opacity = scale(loading, 0, 100, 1, 0);
+    bg.style.filter = `blur(${scale(loading, 0, 100, 30, 0)}px`
     console.log(loading);
+};
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 };
 
 let int = setInterval(blurring, 30);
